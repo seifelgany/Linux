144d009be2c6976e61741ac34f8a838806a182fd
seif el_dein mohamed <selgany@gmail.com>
2023-05-11 17:06:36 +0300

    modified chatgpt file

--- Python/tasks & projects/Connect_ChatGpt.py
+++ Python/tasks & projects/Connect_ChatGpt.py
@@ -2,7 +2,7 @@ import openai
 openai.api_key = "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"
 model_engine = "gpt-3.5-turbo"
 
-
+print ("hello word")
 
 response = openai.ChatCompletion.create(
     model='gpt-3.5-turbo',
